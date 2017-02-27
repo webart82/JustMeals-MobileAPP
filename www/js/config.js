d960122a630b7de3254b846cfbc45249dd256f98
yuvankarthik <karthikjune90@gmail.com>
2017-02-27 22:47:49 +0530

    Add files via upload

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 
 var krms_config ={	
-	'ApiUrl' : "https://justmeals.co.uk/mobileapp/api",
-	'DialogDefaultTitle' : "Just Meals",
+	'ApiUrl' : "YOUR_API_URL",
+	'DialogDefaultTitle' : "YOUR_OWN_DIALOG_TITLE",
 	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
 	'facebookAppId' : "YOUR_FACEBOOK_APP_ID",
 	'APIHasKey' : "YOUR API HASH KEY (OPTIONAL)"
