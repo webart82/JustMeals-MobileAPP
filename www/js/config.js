7162b98ee5b8ba138aca43524d42579452f13fba
yuvankarthik <karthikjune90@gmail.com>
2017-02-28 22:38:30 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -3,6 +3,4 @@ var krms_config ={
 	'ApiUrl' : "https://justmeals.co.uk/mobileapp/api",
 	'DialogDefaultTitle' : "Just Meals",
 	'pushNotificationSenderid' : "309055650516",
-	'facebookAppId' : "YOUR_FACEBOOK_APP_ID",
-	'APIHasKey' : "YOUR API HASH KEY (OPTIONAL)"
 };
