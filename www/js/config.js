68bd7ab092a4c303c9b4daf907abee857ccf44a3
yuvankarthik <karthikjune90@gmail.com>
2017-03-10 00:04:36 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -3,4 +3,5 @@ var krms_config ={
 	'ApiUrl' : "https://justmeals.co.uk/mobileapp/api",
 	'DialogDefaultTitle' : "Just Meals",
 	'pushNotificationSenderid' : "309055650516",
+	'APIHasKey' : "cec87242c204dbd33e2b413ef532f3b1"
 };
